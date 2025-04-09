bb821f3e52d2c4667031db4edc28695ea7ee843b
designhuh <designhuh84@gmail.com>
2025-04-10 01:55:24 +0900

    commit

--- js/ui.js
+++ js/ui.js
@@ -14,7 +14,14 @@ class GlobalHeaderUI extends HTMLElement {
         });
     }
     onload() {
-
+        document.addEventListener('mousemove', event => {
+            if(event.target.closest('nav') || event.target.closest('global-header-menu')){
+                document.querySelector('global-header-menu').classList.add('on');
+            }else{
+                document.querySelector('global-header-menu').classList.remove('on');
+            }
+        });
+        
     }
 }
 customElements.define('global-header', GlobalHeaderUI);
@@ -25,11 +32,11 @@ GlobalHeaderUI.html = `
             <img src="./image/logo.svg">
         </a>
         <nav>
-            <a href="./">배출사업장</a>
-            <a href="./">실태조사</a>
-            <a href="./">통계현황</a>
-            <a href="./">민원서비스</a>
-            <a href="./">고객센터</a>
+            <a href="./sub01.html">배출사업장</a>
+            <a href="#!">실태조사</a>
+            <a href="./sub03.html">통계현황</a>
+            <a href="#!">민원서비스</a>
+            <a href="#!">고객센터</a>
         </nav>
         <button type="button" class="btn-header-login"></button>
     </div>
@@ -103,3 +110,70 @@ class MainComplaintsUI extends HTMLElement {
     }
 }
 customElements.define('main-complaints', MainComplaintsUI);
+
+class GlobalHeaderMenuUI extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = GlobalHeaderMenuUI.html;
+    }
+
+}
+customElements.define('global-header-menu', GlobalHeaderMenuUI);
+
+GlobalHeaderMenuUI.html = `
+    <div class="inner">
+
+        <div class="col">
+            <div class="category">배출사업장</div>
+            <div class="sub-category">사업장정보관리</div>
+            <a href="./sub01.html" class="menu">사업장관리</a>
+            <a href="#!" class="menu">지도점검 및 행정처분</a>
+        </div>
+
+        <div class="col">
+            <div class="category">실태조사</div>
+            <div class="sub-category">실태조사</div>
+            <a href="#!" class="menu">지자체 실태조사</a>
+        </div>
+
+        <div class="col">
+            <div class="category">통계현황</div>
+            <div class="sub-category">배출사업장현황</div>
+            <a href="./sub03.html" class="menu">악취배출사업장</a>
+        </div>
+
+        <div class="col">
+            <div class="category">민원서비스</div>
+            <div class="sub-category">민원관리</div>
+            <a href="#!" class="menu">민원접수</a>
+            <a href="#!" class="menu">현장조사</a>
+            <a href="#!" class="menu">시료채취</a>
+            <a href="#!" class="menu">행정처분</a>
+        </div>
+
+        <div class="col">
+            <div class="category">고객센터</div>
+            <div class="sub-category">고객센터</div>
+            <a href="#!" class="menu">공지사항</a>
+            <a href="#!" class="menu">FAQ</a>
+            <a href="#!" class="menu">자료실</a>
+            <a href="#!" class="menu">우수사례</a>
+        </div>
+
+    </div>
+`;
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.tab-row .tab').forEach(tab => {
+        tab.addEventListener('click', event => {
+            const tabName = event.target.dataset.name;
+            document.querySelectorAll('.tab-row .tab').forEach(tab => { tab.classList.remove('on') });
+            document.querySelector('.tab-row .tab[data-name="' + tabName + '"]').classList.add('on');
+            document.querySelectorAll('.tab-contents').forEach(tabContents => { tabContents.classList.remove('on') });
+            document.querySelector('.tab-contents[data-name="' + tabName + '"]').classList.add('on');
+        });
+    });
+});
